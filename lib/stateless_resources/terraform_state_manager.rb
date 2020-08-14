285d1a4e7245476c4f9258e0243f7abe3a0bb1a1
David Salgado <david@digitalronin.com>
2020-08-14 15:49:50 +0100

    Remove unneeded begin/rescue around download loop

--- lib/stateless_resources/terraform_state_manager.rb
+++ lib/stateless_resources/terraform_state_manager.rb
@@ -13,13 +13,9 @@ module StatelessResources
       s3_keys_list = s3client.bucket("cloud-platform-terraform-state").objects(prefix:'cloud-platform-network/', delimiter: '').collect(&:key)
 
       s3_keys_list.each do |each_key|
-        begin
-          #extract the name from the key
-          each_key_list = each_key.split('/')
-          statefile_name_output = "#{dir}/vpc-network-"+each_key_list[1]+".tfstate"
-          download_state_from_s3(s3client, bucket, each_key, statefile_name_output)
-        rescue => e
-        end
+        each_key_list = each_key.split('/')
+        statefile_name_output = "#{dir}/vpc-network-"+each_key_list[1]+".tfstate"
+        download_state_from_s3(s3client, bucket, each_key, statefile_name_output)
       end
     end
 
