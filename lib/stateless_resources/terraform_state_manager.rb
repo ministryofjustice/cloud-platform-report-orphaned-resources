2d9e03d9c3f915f6f12aed303b296343d4776387
David Salgado <david@digitalronin.com>
2020-08-21 15:14:59 +0100

    Expose a "download_terraform_state" method
    This allows us to download a single terraform
    statefile, given an s3 bucket key

--- lib/stateless_resources/terraform_state_manager.rb
+++ lib/stateless_resources/terraform_state_manager.rb
@@ -11,15 +11,16 @@ module StatelessResources
 
     def download_files
       keys = s3client.bucket("cloud-platform-terraform-state").objects(prefix: "cloud-platform-network/", delimiter: "").collect(&:key)
+      keys.map { |key| download_terraform_state(key) }
+    end
 
-      keys.map do |key|
-        name = key.split("/")[1] # e.g. "cloud-platform-network/live-1/terraform.tfstate" -> "live-1"
-        outfile = "#{dir}/#{name}.tfstate"
-        unless FileTest.exists?(outfile)
-          s3client.bucket(bucket).object(key).get(response_target: outfile)
-        end
-        outfile
+    def download_terraform_state(key)
+      name = key.split("/")[-2] # e.g. "cloud-platform-network/live-1/terraform.tfstate" -> "live-1"
+      outfile = "#{dir}/#{name}.tfstate"
+      unless FileTest.exists?(outfile)
+        s3client.bucket(bucket).object(key).get(response_target: outfile)
       end
+      outfile
     end
   end
 end
