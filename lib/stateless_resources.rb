e3be496fdeb679529525c72553678280a47879ef
David Salgado <david@digitalronin.com>
2020-09-02 15:04:32 +0100

    Remove unneeded requires and old code

--- lib/stateless_resources.rb
+++ lib/stateless_resources.rb
@@ -1,12 +1,9 @@
 require "pry-byebug"
 require "bundler/setup"
 require "json"
-require "find"
 require "aws-sdk-ec2"
 require "aws-sdk-s3"
-require "aws-sdk-autoscaling"
 require "aws-sdk-route53"
-require "slack-notifier"
 
 require_relative "./stateless_resources/lister"
 require_relative "./stateless_resources/terraform_state_manager"
